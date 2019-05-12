751bd101d46223aa5b4528b05dc88b05f0dcf46d
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-05-12 21:43:23 +0200

    Zmienianie ststusu telewizora

---
+++ src/Televisor.java
@@ -0,0 +1,20 @@
+public class Televisor {
+    boolean status;
+
+    void turnOn(){
+        status = true;
+    }
+
+
+    void turnOff( ){
+        status = false;
+
+    }
+
+    void showStatus(){
+        System.out.println(status);
+
+    }
+
+}
+
---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+
+        Televisor televisior1 = new Televisor();
+
+        televisior1.showStatus();
+        televisior1.turnOn();
+
+        televisior1.showStatus();
+        televisior1.turnOff();
+        televisior1.showStatus();
+    }
+
+}
